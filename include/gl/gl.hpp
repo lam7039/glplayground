244dc8feb4bea2140ddf196bedbd8c72a6fdece5
lamram <kelvin7039@gmail.com>
2025-04-30 23:33:24 +0200

    use glGetError for debug breaks on exact error lines

--- include/gl/gl.hpp
+++ include/gl/gl.hpp
@@ -1,29 +1,30 @@
 #pragma once
 
 #include <iostream>
-#include <signal.h>
 
 #include "glad/gl.h"
 
 
-// #if NDEBUG
+#if NDEBUG
     #define CHECK_GL_ERROR(GL_CALL) GL_CALL;
-// #else
-//     #ifdef _WIN32
-//         #define ASSERT(x) if (!(x)) __debugbreak();
-//     #else
-//         #define ASSERT(x) if (!(x)) raise(SIGTRAP);
-//     #endif
-//     #define CHECK_GL_ERROR(GL_CALL) \
-//         GL_CALL;\
-//         ASSERT(GLLogCall(#GL_CALL, __FILE__, __LINE__))
-// #endif
+#else
+    #ifdef _WIN32
+        #include <intrin.h>
+        #define ASSERT(x) if (!(x)) __debugbreak();
+    #else
+        #include <csignal>
+        #define ASSERT(x) if (!(x)) raise(SIGTRAP);
+    #endif
+    #define CHECK_GL_ERROR(GL_CALL) \
+        GL_CALL;\
+        ASSERT(GLLogCall(#GL_CALL, __FILE__, __LINE__))
+#endif
 
-// static bool GLLogCall(const char* function, const char* file, int line) {
-//     GLenum error = glGetError();
-//     if (error != GL_NO_ERROR) {
-//         std::printf("[OpenGL Error] (%d): %s %s %d\n", error, function, file, line);
-//         return false;
-//     }
-//     return true;
-// }
+static bool GLLogCall(const char* function, const char* file, int line) {
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        std::printf("[OpenGL Error] (%d): %s %s %d\n", error, function, file, line);
+        return false;
+    }
+    return true;
+}
