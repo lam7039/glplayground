3adab4a88d76acc7e5c4b85560850e9c6a785c58
lamram <kelvin7039@gmail.com>
2025-06-08 15:20:28 +0200

    rm old main comment

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include "window.hpp"
+// #include "imgui.hpp"
 #include "renderer.hpp"
 #include "gl/gl_renderer.hpp"
 #include "game.hpp"
@@ -6,6 +7,7 @@
 
 int main(int argc, char** argv) {
     Window window("glplayground");
+    // ImGuiWrapper imgui(window.size());
 
     Renderer renderer;
     renderer.set_renderer(std::make_unique<GLRenderer>());
@@ -14,16 +16,22 @@ int main(int argc, char** argv) {
     Game game(argv[0], renderer);
     game.init(window.size());
 
+    // imgui.attach(window.instance());
+
     while (window.running()) {
         game.update();
 
         renderer.clear();
         game.render();
 
+        // imgui.set(entities);
+        // imgui.render();
+
         window.swap();
         window.poll_events();
     }
 
+    // imgui.detach();
     game.quit();
     renderer.destroy();
     window.destroy();
@@ -32,53 +40,3 @@ int main(int argc, char** argv) {
 }
 
 //TODO: fetch third party libraries with cmake instead of saving them in the repo
-
-
-
-
-
-
-
-
-
-// #include "window.hpp"
-// // #include "imgui.hpp"
-// #include "game.hpp"
-// #include "renderer.hpp"
-// #include "gl/gl_renderer.hpp"
-
-// // TODO: move .hpp to source unless they're for a public API
-// int main(int argc, char** argv) {
-//     Window window("glplayground");
-//     // ImGuiWrapper imgui(window.size());
-
-//     Renderer renderer;
-//     renderer.set_renderer(std::make_unique<GLRenderer>());
-//     renderer.init();
-
-//     Game game(argv[0], renderer);
-//     game.init(window.size());
-
-//     // imgui.attach(window.instance());
-
-//     while (window.running()) {
-//         game.update();
-//         game.render();
-
-//         // imgui.set(entities);
-//         // imgui.render();
-
-//         renderer.clear();
-
-//         window.swap();
-//         window.poll_events();
-//     }
-
-//     // imgui.detach();
-
-//     renderer.destroy();
-//     game.quit();
-//     window.destroy();
-    
-//     return 0;
-// }
